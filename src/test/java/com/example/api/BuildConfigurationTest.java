08a53f23bb300969f1e6c6e5e00572bd061971a3
ekaterinafedorova <ekaterinad.fedorova@gmail.com>
2024-10-13 00:28:11 +0300

    Add test cases with allure steps

--- src/test/java/com/example/api/BuildConfigurationTest.java
+++ src/test/java/com/example/api/BuildConfigurationTest.java
@@ -1,24 +1,53 @@
 package com.example.api;
 
-import com.example.teamcity.api.models.User;
-import com.example.teamcity.api.spec.Specifications;
-import io.restassured.RestAssured;
-import org.apache.http.HttpStatus;
 import org.testng.annotations.Test;
 
-public class BuildConfigurationTest extends BaseApiTest{
-    @Test
-    public void buildConfigurationTest() {
-        var user = User.builder()
-                .username("admin")
-                .password("admin")
-                .build();
-        var token = RestAssured
-                .given()
-                .spec(Specifications.getSpec().authSpec(user))
-                .get("/authenticationTest.html?crsf")
-                .then().assertThat().statusCode(HttpStatus.SC_OK)
-                .extract().asString();
-        System.out.println(token);
+import static io.qameta.allure.Allure.step;
+
+@Test(groups = {"Regression"})
+public class BuildConfigurationTest extends BaseApiTest {
+    @Test(description = "User should be able to create build type", groups = {"Positive", "CRUD"})
+    public void userCreateBuildTypeTest() {
+        step("Create user");
+        step("Create project");
+        step("Create buildType for project by user");
+        step("Check buildType was created successfully with correct data");
+
+    }
+
+    @Test(description = "User should not be able to create two build types with the same id", groups = {"Negative", "CRUD"})
+    public void userCreateTwoBuildTypeWithTheSameIdTest() {
+        step("Create user");
+        step("Create project");
+        step("Create buildType1 for project by user");
+        step("Create buildType2 with the same id as buildType1 for project by user");
+        step("Check buildType2 was not created with bad request code");
+
+    }
+
+    @Test(description = "Project admin should be able to create build type for their project", groups = {"Positive", "Roles"})
+    public void projectAdminCreateBuildTypeTest() {
+        step("Create user");
+        step("Create project");
+        step("Grant user PROJECT_ADMIN role in project");
+        step("Create buildType for project by user");
+        step("Create buildType with the same id as buildType1 for project by user (PROJECT_ADMIN)");
+        step("Check buildType was created successfully");
+
+    }
+
+    @Test(description = "Project admin should not be able to create build type for not their project", groups = {"Negative", "Roles"})
+    public void projectAdminCreateBuildTypeForAnotherUserProjectTest() {
+        step("Create user1");
+        step("Create project1");
+        step("Grant user1 PROJECT_ADMIN role in project1");
+
+        step("Create user2");
+        step("Create project2");
+        step("Grant user2 PROJECT_ADMIN role in project1");
+
+        step("Create buildType for project1 by user2");
+        step("Check buildType2 was not created with forbidden code");
+
     }
 }
