7cdee5eb1a6c8d3b3513100d1e2c357a972b5e45
ekaterinafedorova <ekaterinad.fedorova@gmail.com>
2024-10-13 21:20:24 +0300

    Add create user, random data generation, build configuration test

--- src/test/java/com/example/api/BaseTest.java
+++ src/test/java/com/example/api/BaseTest.java
@@ -1,4 +1,19 @@
 package com.example.api;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.asserts.SoftAssert;
+
 public class BaseTest {
+    protected SoftAssert softy;
+
+    @BeforeTest(alwaysRun = true)
+    public void beforeTest(){
+        softy = new SoftAssert();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void afterTest(){
+        softy.assertAll();
+    }
 }
---
+++ src/test/java/com/example/api/BuildTypeTest.java
@@ -0,0 +1,87 @@
+package com.example.api;
+
+import com.example.teamcity.api.enums.Endpoint;
+import com.example.teamcity.api.models.BuildType;
+import com.example.teamcity.api.models.Project;
+import com.example.teamcity.api.models.User;
+import com.example.teamcity.api.requests.checked.CheckedBase;
+import com.example.teamcity.api.spec.Specifications;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import static com.example.teamcity.api.generators.TestDataGenerator.generate;
+import static io.qameta.allure.Allure.step;
+
+@Test(groups = {"Regression"})
+public class BuildTypeTest extends BaseApiTest {
+
+    @Test(description = "User should be able to create build type", groups = {"Positive", "CRUD"})
+    public void userCreatesBuildTypeTest() {
+        var user = generate(User.class);
+
+        step("Create user", () ->  {
+            var requester = new CheckedBase<User>(Specifications.superUserSpec(), Endpoint.USERS);
+            requester.create(user);
+        });
+
+        var project = generate(Project.class);
+        AtomicReference<String> projectId = new AtomicReference<>("");
+
+        step("Create project by user", () -> {
+            var requester = new CheckedBase<Project>(Specifications.authSpec(user), Endpoint.PROJECTS);
+            projectId.set(requester.create(project).getId());
+        });
+
+        var buildType = generate(BuildType.class);
+        buildType.setProject(Project.builder().id(projectId.get()).locator(null).build());
+
+        var requester = new CheckedBase<BuildType>(Specifications.authSpec(user), Endpoint.BUILD_TYPES);
+        AtomicReference<String> buildTypeId = new AtomicReference<>("");
+
+        step("Create buildType for project by user", () -> {
+            buildTypeId.set(requester.create(buildType).getId());
+        });
+
+        step("Check buildType was created successfully with correct data", () ->  {
+            var createdBuildType = requester.read(buildTypeId.get());
+            softy.assertEquals(buildType.getName(), createdBuildType.getName(), "Build type name is not correct");
+        });
+    }
+
+    @Test(description = "User should not be able to create two build types with the same id", groups = {"Negative", "CRUD"})
+    public void userCreateTwoBuildTypeWithTheSameIdTest() {
+        step("Create user");
+        step("Create project");
+        step("Create buildType1 for project by user");
+        step("Create buildType2 with the same id as buildType1 for project by user");
+        step("Check buildType2 was not created with bad request code");
+
+    }
+
+    @Test(description = "Project admin should be able to create build type for their project", groups = {"Positive", "Roles"})
+    public void projectAdminCreateBuildTypeTest() {
+        step("Create user");
+        step("Create project");
+        step("Grant user PROJECT_ADMIN role in project");
+        step("Create buildType for project by user");
+        step("Create buildType with the same id as buildType1 for project by user (PROJECT_ADMIN)");
+        step("Check buildType was created successfully");
+
+    }
+
+    @Test(description = "Project admin should not be able to create build type for not their project", groups = {"Negative", "Roles"})
+    public void projectAdminCreateBuildTypeForAnotherUserProjectTest() {
+        step("Create user1");
+        step("Create project1");
+        step("Grant user1 PROJECT_ADMIN role in project1");
+
+        step("Create user2");
+        step("Create project2");
+        step("Grant user2 PROJECT_ADMIN role in project1");
+
+        step("Create buildType for project1 by user2");
+        step("Check buildType2 was not created with forbidden code");
+
+    }
+}
